243b31e398f9e70738cf6f735db2ffe66ec89d56
Brionconroy <>
2023-05-03 16:10:37 +0000

    Add js to class a random number for the cumputer and to display user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
-function runGame() {
+const userChoicePick = document.getElementById("user-pick");
+const computerChoicePick = document.getElementById("computer-pick");
+const displayAnswer = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+let computerChoice;
 
-}
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (event) => {
+    userChoice = event.target.id;
+    userChoicePick.innerHTML = userChoice;
+    randomComputerChoice();
+}));
 
-function checkAnswer() {
-    
-}
+// random number generated for computer from 0-2 //
+
+function randomComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        computerChoice = "Venusaur";
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Blastoise";
+    }
+    if (randomNumber === 3) {
+        computerChoice = "Charizard";
+    }
+    computerChoicePick.innerHTML = computerChoice;
+};
