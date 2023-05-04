044aef5b88b7873f260e1f66107dfc1a4c2d5c42
Brionconroy <>
2023-05-04 12:46:37 +0000

    Add results js code and renamed some of the ID's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
 const userChoicePick = document.getElementById("user-pick");
 const computerChoicePick = document.getElementById("computer-pick");
-const displayAnswer = document.getElementById("result");
-const possibleChoices = document.querySelectorAll("button");
+const displayBattleResult = document.getElementById("result");
+const trainersPossibleChoice = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
+let result;
 
 // using event listener to pick the users choice and display it in the score area //
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
-    userChoice = e.target.id;
+trainersPossibleChoice.forEach(button => button.addEventListener("click", (event) => {
+    userChoice = event.target.id;
     userChoicePick.innerHTML = userChoice;
     randomComputerChoice();
+    battleResult();
 }));
 
 // random number generated for computer from 0-2 //
 
 function randomComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
         computerChoice = "Venusaur";
     }
@@ -28,3 +30,30 @@ function randomComputerChoice() {
     }
     computerChoicePick.innerHTML = computerChoice;
 };
+
+// displays result in result section //
+
+function battleResult() {
+    if (userChoice === computerChoice) {
+        result = "Your Pokemon were evenly matched";
+    }
+    if (userChoice === "Venusaur" && computerChoice === "Blastoise") {
+        result = "Winner";
+    }
+    if (userChoice === "Venusaur" && computerChoice === "Charizard") {
+        result = "Loser";
+    }
+    if (userChoice === "Blastoise" && computerChoice === "Venusaur") {
+        result = "Loser";
+    }
+    if (userChoice === "Blastoise" && computerChoice === "Charizard") {
+        result = "Winner";
+    }
+    if (userChoice === "Charizard" && computerChoice === "Venusaur") {
+        result = "Winner";
+    }
+    if (userChoice === "Charizard" && computerChoice === "Blastoise") {
+        result = "Loser";
+    }
+    displayBattleResult.innerHTML = result;
+}
